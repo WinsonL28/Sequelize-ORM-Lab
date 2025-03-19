ca49164b4dfaa5afbd4317f158d3d3112ad6d621
WinsonL28 <winsonliang@live.com>
2025-03-18 20:15:37 -0700

    completed Sequelize Lab

---
+++ models/pet_type.js
@@ -0,0 +1,19 @@
+const {Sequelize, DataTypes} = require('sequelize');
+const databaseConnectionString = include('/databaseConnectionSequelize');
+const sequelize = new Sequelize(databaseConnectionString);
+
+const pet_typeModel = sequelize.define('pet_type',
+    {
+        pet_type_id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+        type: {type: Sequelize.STRING, allowNull: false, unique: true},
+    },
+
+    {
+        tableName: 'pet_type',
+        timestamps: false,
+        singular: 'pet_type',
+        plural: 'pet_type'
+    }
+);
+
+module.exports = pet_typeModel;
